baeb27dd7a070a53d1bc29918693b4aa8131e678
alexpeloquin <46466270+alexpeloquin@users.noreply.github.com>
2019-02-23 11:49:58 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+var DB = [];
+
+function Todo(text,imp) {
+    this.text = text;
+    this.imp = imp;
+}
+
+$(document).ready(function(){
+    $('#btnAdd').click(createTodo);
+    $('#txtText').keypress(function(e){
+        if(e.keyCode==13){
+            createTodo();}
+    });
+    var h1JQ = $('#mainH1');
+    h1JQ.addClass('test');
+    console.log(h1JQ.text());
+    console.log(h1JQ.html());
+});
+
+function createTodo(){
+    var text = $('#txtText').val();
+    var imp;
+    var isRd1checked = $('#rd1').prop('checked');
+    var isRd2checked = $('#rd2').prop('checked');
+    var isRd3checked = $('#rd3').prop('checked');
+    if (isRd1checked) {
+        imp = 'High';
+    } else if (isRd2checked) {
+        imp = 'Medium';
+    } else if (isRd3checked) {
+        imp = 'Low';
+    }
+    console.log(imp);
+    var objTodo = new Todo(text,imp);
+    DB.push(objTodo);
+    var li = `<li class="list-group-item list-group-item-success">
+        ${text} - ${imp}
+        <button class='btn-right btn btn-danger btn-sm'>Remove</button>
+        </li>`;
+    $('#list').append(li);
+    $('#txtText').val('');
+    $('#txtText').focus();
+}
